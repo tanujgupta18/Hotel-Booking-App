ae5eaf886846bfc24b57940e404daf9c48200d0a
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-13 11:39:37 +0530

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return <div>{!isOwnerPath && <Navbar />}</div>;
+};
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import searchIcon from "./searchIcon.svg";
+import closeIcon from "./closeIcon.svg";
+import menuIcon from "./menuIcon.svg";
+
+export const assets = {
+  logo,
+  searchIcon,
+  closeIcon,
+  menuIcon,
+};
