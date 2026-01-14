8a4caf7d17b3fd984bacf2898e2119d7913aaf0f
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-14 19:10:15 +0530

    Assets Added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,12 @@ import closeIcon from "./closeIcon.svg";
 import menuIcon from "./menuIcon.svg";
 import heroImage from "./heroImage.png";
 import calenderIcon from "./calenderIcon.svg";
+import starIconFilled from "./starIconFilled.svg";
+import locationIcon from "./locationIcon.svg";
+import roomImg1 from "./roomImg1.png";
+import roomImg2 from "./roomImg2.png";
+import roomImg3 from "./roomImg3.png";
+import roomImg4 from "./roomImg4.png";
 
 export const assets = {
   logo,
@@ -12,6 +18,87 @@ export const assets = {
   menuIcon,
   heroImage,
   calenderIcon,
+  starIconFilled,
+  locationIcon,
 };
 
 export const cities = ["Dubai", "Singapore", "New York", "London"];
+
+// User Dummy Data
+export const userDummyData = {
+  _id: "user_2unqyL4diJFP1E3pIBnasc7w8hP",
+  username: "Great Stack",
+  email: "user.greatstack@gmail.com",
+  image:
+    "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+  role: "hotelOwner",
+  createdAt: "2025-03-25T09:29:16.367Z",
+  updatedAt: "2025-04-10T06:34:48.719Z",
+  __v: 1,
+  recentSearchedCities: ["New York"],
+};
+
+// Hotel Dummy Data
+export const hotelDummyData = {
+  _id: "67f76393197ac559e4089b72",
+  name: "Urbanza Suites",
+  address: "Main Road  123 Street , 23 Colony",
+  contact: "+0123456789",
+  owner: userDummyData,
+  city: "New York",
+  createdAt: "2025-04-10T06:22:11.663Z",
+  updatedAt: "2025-04-10T06:22:11.663Z",
+  __v: 0,
+};
+
+// Rooms Dummy Data
+export const roomsDummyData = [
+  {
+    _id: "67f7647c197ac559e4089b96",
+    hotel: hotelDummyData,
+    roomType: "Double Bed",
+    pricePerNight: 399,
+    amenities: ["Room Service", "Mountain View", "Pool Access"],
+    images: [roomImg1, roomImg2, roomImg3, roomImg4],
+    isAvailable: true,
+    createdAt: "2025-04-10T06:26:04.013Z",
+    updatedAt: "2025-04-10T06:26:04.013Z",
+    __v: 0,
+  },
+  {
+    _id: "67f76452197ac559e4089b8e",
+    hotel: hotelDummyData,
+    roomType: "Double Bed",
+    pricePerNight: 299,
+    amenities: ["Room Service", "Mountain View", "Pool Access"],
+    images: [roomImg2, roomImg3, roomImg4, roomImg1],
+    isAvailable: true,
+    createdAt: "2025-04-10T06:25:22.593Z",
+    updatedAt: "2025-04-10T06:25:22.593Z",
+    __v: 0,
+  },
+  {
+    _id: "67f76406197ac559e4089b82",
+    hotel: hotelDummyData,
+    roomType: "Double Bed",
+    pricePerNight: 249,
+    amenities: ["Free WiFi", "Free Breakfast", "Room Service"],
+    images: [roomImg3, roomImg4, roomImg1, roomImg2],
+    isAvailable: true,
+    createdAt: "2025-04-10T06:24:06.285Z",
+    updatedAt: "2025-04-10T06:24:06.285Z",
+    __v: 0,
+  },
+  {
+    _id: "67f763d8197ac559e4089b7a",
+    hotel: hotelDummyData,
+    roomType: "Single Bed",
+    pricePerNight: 199,
+    amenities: ["Free WiFi", "Room Service", "Pool Access"],
+    images: [roomImg4, roomImg1, roomImg2, roomImg3],
+    isAvailable: true,
+    createdAt: "2025-04-10T06:23:20.252Z",
+    updatedAt: "2025-04-10T06:23:20.252Z",
+    __v: 0,
+  },
+];
