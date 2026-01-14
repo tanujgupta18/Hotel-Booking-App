ef9ed3f426522b7ae2a717fd031cd7232aa5c7e9
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-14 16:16:50 +0530

    Featured Destination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import FeaturedDestination from "../components/FeaturedDestination";
 import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
     </>
   );
 };
