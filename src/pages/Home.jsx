8ba25abc6da8a0e411636bf215a3ce307a0a3a97
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-16 11:40:39 +0530

    Newsletter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import FeaturedDestination from "../components/FeaturedDestination";
 import Hero from "../components/Hero";
+import Newsletter from "../components/Newsletter";
 import Testimonial from "../components/Testimonial";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
       <FeaturedDestination />
       <ExclusiveOffers />
       <Testimonial />
+      <Newsletter />
     </>
   );
 };
