59893cce738977daa1df589104d1f97206fb251c
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-18 10:59:49 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+connectDB();
+
+const app = express();
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+
+app.get("/", (req, res) => res.send("API is working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
