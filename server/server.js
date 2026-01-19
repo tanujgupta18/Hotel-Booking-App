b844330bcc492a2d5bc85257f8e3bc07fc7c8911
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-19 18:28:17 +0530

    Rooms API

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
 
 connectDB();
 connectCloudinary();
@@ -24,6 +25,7 @@ app.use("/api/clerk", clerkWebhooks);
 app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
 
 const PORT = process.env.PORT || 3000;
 
