22e654e3057ac81617cacbe86ea4bee9753ffd14
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-19 12:19:49 +0530

    Cloudinary Setup

--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
