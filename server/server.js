2e9d4e4d347290d37f07d2de64e3c149286ee6e4
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-19 11:48:02 +0530

    Hotels API

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
 
 connectDB();
 
@@ -20,6 +21,7 @@ app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
 
 const PORT = process.env.PORT || 3000;
 
