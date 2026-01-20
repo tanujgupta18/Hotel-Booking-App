d182590e17f4f7b5cc18864e98fa16aeda1bf11f
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-20 18:02:55 +0530

    Hotel Reg Form

--- Client/src/components/HotelReg.jsx
+++ Client/src/components/HotelReg.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const HotelReg = () => {
+  const { setShowHotelReg, axios, getToken, setIsOwner } = useAppContext();
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    try {
+      event.preventDefault();
+      const { data } = await axios.post(
+        `/api/hotels/`,
+        { name, contact, address, city },
+        { headers: { Authorization: `Bearer ${await getToken()}` } },
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setIsOwner(true);
+        setShowHotelReg(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70">
-      <form className="flex bg-white rounded-xl max-w-4xl max-md:mx-2">
+    <div
+      onClick={() => setShowHotelReg(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex bg-white rounded-xl max-w-4xl max-md:mx-2"
+      >
         <img
           src={assets.regImage}
           alt="reg-image"
@@ -12,6 +48,7 @@ const HotelReg = () => {
         />
         <div className="relative flex flex-col items-center md:w-1/2 p-8 md:p-10">
           <img
+            onClick={() => setShowHotelReg(false)}
             src={assets.closeIcon}
             alt="close-icon"
             className="absolute top-4 right-4 h-4 w-4 cursor-pointer"
@@ -25,6 +62,8 @@ const HotelReg = () => {
             </label>
             <input
               id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
@@ -39,6 +78,8 @@ const HotelReg = () => {
             </label>
             <input
               id="contact"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
               type="text"
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
@@ -53,6 +94,8 @@ const HotelReg = () => {
             </label>
             <input
               id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
               type="text"
               placeholder="Type here"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
@@ -67,6 +110,8 @@ const HotelReg = () => {
             </label>
             <select
               id="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
             >
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { useClerk, UserButton } from "@clerk/clerk-react";
+import { useAppContext } from "../context/AppContext";
 
 const BookIcon = () => (
   <svg
@@ -35,9 +36,10 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { openSignIn } = useClerk();
-  const { user } = useUser();
-  const navigate = useNavigate();
+
   const location = useLocation();
+
+  const { user, navigate, isOwner, setShowHotelReg } = useAppContext();
 
   useEffect(() => {
     if (location.pathname !== "/") {
@@ -75,9 +77,9 @@ const Navbar = () => {
       {/* Desktop Nav */}
       <div className="hidden md:flex items-center gap-4 lg:gap-8">
         {navLinks.map((link, i) => (
-          <a
+          <Link
             key={i}
-            href={link.path}
+            to={link.path}
             className={`group flex flex-col gap-0.5 ${
               isScrolled ? "text-gray-700" : "text-white"
             }`}
@@ -88,18 +90,21 @@ const Navbar = () => {
                 isScrolled ? "bg-gray-700" : "bg-white"
               } h-0.5 w-0 group-hover:w-full transition-all duration-300`}
             />
-          </a>
+          </Link>
         ))}
-        <button
-          onClick={() => {
-            navigate("/owner");
-          }}
-          className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
-            isScrolled ? "text-black" : "text-white"
-          } transition-all`}
-        >
-          Dashboard
-        </button>
+
+        {user && (
+          <button
+            onClick={() => {
+              isOwner ? navigate("/owner") : setShowHotelReg(true);
+            }}
+            className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
+              isScrolled ? "text-black" : "text-white"
+            } transition-all`}
+          >
+            {isOwner ? "Dashboard" : "List Your Hotel"}
+          </button>
+        )}
       </div>
 
       {/* Desktop Right */}
@@ -172,19 +177,19 @@ const Navbar = () => {
         </button>
 
         {navLinks.map((link, i) => (
-          <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)}>
+          <Link key={i} to={link.path} onClick={() => setIsMenuOpen(false)}>
             {link.name}
-          </a>
+          </Link>
         ))}
 
         {user && (
           <button
             onClick={() => {
-              navigate("/owner");
+              isOwner ? navigate("/owner") : setShowHotelReg(true);
             }}
             className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
           >
-            Dashboard
+            {isOwner ? "Dashboard" : "List Your Hotel"}
           </button>
         )}
 
