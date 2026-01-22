bf70bde19dded4d54e7df863f96395bbd9f07a28
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-22 14:40:41 +0530

    Fix

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -13,7 +13,7 @@ export const stripeWebhooks = async (req, res) => {
       process.env.STRIPE_WEBHOOK_SECRET,
     );
   } catch (error) {
-    res.status(400).send(`Webhook Error: ${error.message}`);
+    return res.status(400).send(`Webhook Error: ${error.message}`);
   }
 
   // Handle the event
