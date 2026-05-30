154374fc52c699331bba27adee103f6ffd541b2b
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-30 17:20:34 +0530

    Auth Fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,24 @@
 import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
-  const { userId } = req.auth;
+  const { userId } = req.auth();
+
   if (!userId) {
-    res.json({ success: false, message: "not authenticated" });
-  } else {
-    const user = await User.findById(userId);
-    req.user = user;
-    next();
+    return res.json({
+      success: false,
+      message: "Not authenticated",
+    });
   }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  req.user = user;
+  next();
 };
